6fb9a5b4aee7965df20e93ed2a077e0a0922d625
jdgill27 <62246495+jdgill27@users.noreply.github.com>
2020-03-17 00:01:03 +1100

    Add files via upload

---
+++ first-name-blinky.ino
@@ -0,0 +1,259 @@
+
+int led1 = D6;
+int led2 = D7; 
+
+void setup() 
+{
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+}
+
+void loop() 
+{
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(2000);            //Spells 'J'
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(2000);            //Spells 'A'
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(2000);            //Spells 'S'
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(2000);            //Spells 'R'
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(2000);            //Spells 'A'
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(300);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(1000);
+    
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    
+    delay(1500);
+
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+
+    delay(2000);            //Spells 'J'
+
+}
+
+
+
+
+
+
+
+
+// int led1 = D6;
+// int led2 = D7; 
+
+// void setup() 
+// {
+//     pinMode(led1, OUTPUT);
+//     pinMode(led2, OUTPUT);
+// }
+
+// void dot()
+// {
+//     digitalWrite(led1, HIGH);
+//     digitalWrite(led2, HIGH);
+    
+//     delay(300);
+
+//     digitalWrite(led1, LOW);
+//     digitalWrite(led2, LOW);
+
+//     delay(1000);
+// }
+
+// void dash()
+// {
+//     digitalWrite(led1, HIGH);
+//     digitalWrite(led2, HIGH);
+    
+//     delay(1500);
+
+//     digitalWrite(led1, LOW);
+//     digitalWrite(led2, LOW);
+
+//     delay(1000);
+// }
+
+// void loop()
+// {
+//     dash();
+//     dash();
+//     dot();
+//     delay(1000);        //Spells 'G'
+    
+//     dot();
+//     dot();
+//     delay(1000);        //Spells 'I'
+    
+//     dot();
+//     dash();
+//     dot();
+//     dot();
+//     delay(1000);        //Spells 'L'
+    
+//     dot();
+//     dash();
+//     dot();
+//     dot();
+//     delay(1000);        //Spells 'L'
+// }
